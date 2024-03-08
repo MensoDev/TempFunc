129eeec0ebffeeb3793506f971135754a67b9b21
Emerson Trindade (MensoDev) <emerson.trindade@codeblox.com.br>
2024-03-08 18:03:43 -0300

    merge

---
+++ ExemplosInte/ExemplosInte/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+using ExemplosInte;
+
+var pessoa = new Pessoa
+{
+    Idade = 20
+};
+
+var pessoaDiferente = new PessoaDiferente
+{
+    IdadeAno = 1996,
+    IdadeDia = 20,
+    IdadeMes = 1
+};
+
+var pessoaDiferente2 = new PessoaDiferente2
+{
+    Nascimento = DateOnly.FromDateTime(DateTime.Now.AddDays(-18))
+};
+
+//var ehMaiorDeIdade = Validator.PessoaEhMaiorDeIdade(pessoa);
+
+//var ehMaiorDeIdade = Validator.PessoaEhMaiorDeIdade(pessoaDiferente, p => DateTime.Now.Year - p.IdadeAno);
+var ehMaiorDeIdade = Validator.PessoaEhMaiorDeIdade(pessoaDiferente2, p => DateTime.Now.Year - p.Nascimento.Year);
+
+Console.WriteLine($"Pessoa é maior de idade: {ehMaiorDeIdade}");
+
+
+Pessoa[] pessoas = [];
+
+pessoas.ToList().Max
+
+
+
+//Func<string, Pessoa, int> => int QualQuerNome(string qual, Pessoa qual);
+//Action => void QualQuerNome();
+//Action<Pessoa> => void QualQuerNome(Pessoa pessoa);
+//Predicate => bool QualQuerNome();
+//Predicate<Pessoa> => bool QualQuerNome(Pessoa pessoa);
---
+++ ExemplosInte/ExemplosInte/Validator.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+
+namespace ExemplosInte;
+
+internal static class Validator
+{
+    public static bool PessoaEhMaiorDeIdade(Pessoa pessoa)
+    {
+        // Verificar se a pessoa é maior de idade
+        return pessoa.Idade >= 18;
+    }
+
+    // Método que recebe uma pessoa de qual tipo, e retorna boolean
+    // o Segunto parametro é uma func que sabe calcular a idade
+    public static bool PessoaEhMaiorDeIdade<TPessoa>(TPessoa pessoa, Func<TPessoa, int> funcCalculaIdade)
+    {
+        var idade = funcCalculaIdade(pessoa);
+        return idade >= 18;
+    }
+
+    public static void ForEachRefeito(Pessoa[] pessoas, Action<Pessoa> action)
+    {
+        foreach (var pessoa in pessoas)
+        {
+            action(pessoa);
+        }
+    }
+
+}
+
+
+public class Pessoa
+{
+    public int Idade { get; set; }
+}
+
+public class PessoaDiferente
+{
+    public int IdadeDia { get; set; }
+    public int IdadeMes { get; set; }
+    public int IdadeAno { get; set; }
+}
+
+public class PessoaDiferente2
+{
+    public DateOnly Nascimento { get; set; }
+}
